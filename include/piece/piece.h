0e3e33c45a97aeb9674ad1572d589737f5ec8c41
Vaishakh GK <vaishakhgk2006@gmail.com>
2025-02-12 03:02:05 +0530

    feat: Add piece definition

---
+++ include/piece/piece.h
@@ -0,0 +1,22 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+// White pieces
+#define W_PAWN 'P'
+#define W_KNIGHT 'N'
+#define W_BISHOP 'B'
+#define W_ROOK 'R'
+#define W_QUEEN 'Q'
+#define W_KING 'K'
+
+// Black pieces
+#define B_PAWN 'p'
+#define B_KNIGHT 'n'
+#define B_BISHOP 'b'
+#define B_ROOK 'r'
+#define B_QUEEN 'q'
+#define B_KING 'k'
+
+#define EMPTY '_'
+
+#endif
