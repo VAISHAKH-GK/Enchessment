4b4dd6e2f5ba091ab740782b963840bb1b9a7c4d
Vaishakh GK <vaishakhgk2006@gmail.com>
2025-03-02 22:50:52 +0530

    feat: Generate Bishop moves

--- src/moves.c
+++ src/moves.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
 #include "game/moves.h"
 #include "board/board.h"
 #include "board/piece.h"
 #include "game/game.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 void make_move(char *move) {
-  printf("move is - %c%c%c%c\n", move[0], move[1], move[2], move[3]);
   int from_file = file_index(move[0]);
   int from_rank = rank_index(move[1]);
 
@@ -44,7 +43,7 @@ int generate_pawn_moves(int row, int col, char moves[][6]) {
     if (is_inside(second_row, col) && Board[second_row][col] == EMPTY &&
         Board[new_row][col] == EMPTY) {
       sprintf(moves[count++], "%c%c%c%c", 'a' + col, '1' + row, 'a' + col,
-             '1' + second_row);
+              '1' + second_row);
     }
   }
 
@@ -60,10 +59,42 @@ int generate_knight_moves(int row, int col, char moves[][6]) {
     int new_row = row + knight_moves[i][0];
     int new_col = col + knight_moves[i][1];
 
-    if (is_inside(new_row, new_col) && (Board[new_row][new_col] == EMPTY || (turn == WHITE ? is_black(Board[new_row][new_col]) : is_white(Board[new_row][new_col])))) {
+    if (is_inside(new_row, new_col) &&
+        (Board[new_row][new_col] == EMPTY ||
+         (turn == WHITE ? is_black(Board[new_row][new_col])
+                        : is_white(Board[new_row][new_col])))) {
       sprintf(moves[count++], "%c%c%c%c", 'a' + col, '1' + row, 'a' + new_col,
               '1' + new_row);
-        }
+    }
+  }
+
+  return count;
+}
+
+int generate_bishop_moves(int row, int col, char moves[][6]) {
+  int count = 0;
+  int directions[4][2] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+  for (int i = 0; i < 4; i++) {
+    int new_row = row + directions[i][0];
+    int new_col = col + directions[i][1];
+
+    while (is_inside(new_row, new_col)) {
+      if (Board[new_row][new_col] == EMPTY) {
+        sprintf(moves[count++], "%c%c%c%c", 'a' + col, '1' + row, 'a' + new_col,
+                '1' + new_row);
+      } else if (turn == WHITE ? is_black(Board[new_row][new_col])
+                               : is_white(Board[new_row][new_col])) {
+        sprintf(moves[count++], "%c%c%c%c", 'a' + col, '1' + row, 'a' + new_col,
+                '1' + new_row);
+        break;
+      } else {
+        break;
+      }
+
+      new_row += directions[i][0];
+      new_col += directions[i][1];
+    }
   }
 
   return count;
@@ -79,12 +110,15 @@ void white_move() {
   for (int i = 0; i < BOARD_SIZE; i++) {
     for (int j = 0; j < BOARD_SIZE; j++) {
       switch (b[i][j]) {
-        case W_PAWN:
-          move_count += generate_pawn_moves(i, j, moves + move_count);
-          break;
-        case W_KNIGHT:
-          move_count += generate_knight_moves(i, j, moves + move_count);
-          break;
+      case W_PAWN:
+        move_count += generate_pawn_moves(i, j, moves + move_count);
+        break;
+      case W_KNIGHT:
+        move_count += generate_knight_moves(i, j, moves + move_count);
+        break;
+      case W_BISHOP:
+        move_count += generate_bishop_moves(i, j, moves + move_count);
+        break;
       }
     }
   }
@@ -108,12 +142,15 @@ void black_move() {
   for (int i = 0; i < BOARD_SIZE; i++) {
     for (int j = 0; j < BOARD_SIZE; j++) {
       switch (b[i][j]) {
-        case B_PAWN:
-          move_count += generate_pawn_moves(i, j, moves + move_count);
-          break;
-        case B_KNIGHT:
-          move_count += generate_knight_moves(i, j, moves + move_count);
-          break;
+      case B_PAWN:
+        move_count += generate_pawn_moves(i, j, moves + move_count);
+        break;
+      case B_KNIGHT:
+        move_count += generate_knight_moves(i, j, moves + move_count);
+        break;
+      case B_BISHOP:
+        move_count += generate_bishop_moves(i, j, moves + move_count);
+        break;
       }
     }
   }
@@ -129,9 +166,9 @@ void black_move() {
 
 void generate_move() {
   if (turn == WHITE) {
-        white_move();
+    white_move();
   } else {
-        black_move();
+    black_move();
   }
   print_board();
 }
