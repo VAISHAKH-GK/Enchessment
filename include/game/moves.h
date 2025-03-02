75a9e9d833d365d543b822d39a4f8c3b971c8ef8
Vaishakh GK <vaishakhgk2006@gmail.com>
2025-03-02 23:03:16 +0530

    feat: Update moves.h header file

--- include/game/moves.h
+++ include/game/moves.h
@@ -2,6 +2,14 @@
 #define MOVES_H
 
 void make_move(char *move);
+
+int generate_pawn_moves(int row, int col, char moves[][6]);
+int generate_knight_moves(int row, int col, char moves[][6]);
+int generate_bishop_moves(int row, int col, char moves[][6]);
+int generate_rook_moves(int row, int col, char moves[][6]);
+int generate_queen_moves(int row, int col, char moves[][6]);
+int generate_king_moves(int row, int col, char moves[][6]);
+
 void white_move();
 void black_move();
 void generate_move();
